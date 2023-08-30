a784d71890ffad859e50c40ff5b2a340007c8c2c
Hendra <hendrapermanadev@gmail.com>
2023-08-30 22:53:28 +0700

    update-minor

--- src/components/layout/sideMenu.tsx
+++ src/components/layout/sideMenu.tsx
@@ -18,7 +18,7 @@ import { usePathname } from "next/navigation"
 export default function SideMenu() {
 
   const pathname = usePathname()
-  const currentPath = pathname.split("/")[2]
+  const currentPath = pathname.split("/")
   const btnRef = useRef<HTMLButtonElement | null>(null)
 
   const handleClose = () => {
@@ -38,12 +38,22 @@ export default function SideMenu() {
         </SheetHeader>
         <div className="flex flex-col">          
           <div className="flex flex-col">
+            <h1 className="font-semibold mb-4">Pengantar</h1>
+            <div className="flex flex-col space-y-3">
+              <Link onClick={handleClose} href="/about" className={`${currentPath[1] === "about" ? "" : "text-gray-600 dark:text-gray-400"} text-[15px] font-medium`}>
+                About
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">          
+          <div className="flex flex-col">
             <h1 className="font-semibold mb-4">Courses</h1>
             <div className="flex flex-col space-y-3">
-              <Link onClick={handleClose} href="/courses/struktur_data" className={`${currentPath === "struktur_data" ? "" : "text-gray-600 dark:text-gray-400"} text-[15px] font-medium`}>
+              <Link onClick={handleClose} href="/courses/struktur_data" className={`${currentPath[2] === "struktur_data" ? "" : "text-gray-600 dark:text-gray-400"} text-[15px] font-medium`}>
                 Struktur Data
               </Link>
-              <Link onClick={handleClose} href="/courses/algoritma_pemrograman" className={`${currentPath === "algoritma_pemrograman" ? "" : "text-gray-600 dark:text-gray-400"} text-[15px] font-medium`}>
+              <Link onClick={handleClose} href="/courses/algoritma_pemrograman" className={`${currentPath[2] === "algoritma_pemrograman" ? "" : "text-gray-600 dark:text-gray-400"} text-[15px] font-medium`}>
                 Algoritma Pemrograman
               </Link>
             </div>
